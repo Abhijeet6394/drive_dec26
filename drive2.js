9f9e050d3fc8531fdeb65ab55790cba743493979
Abhijeet6394 <92412172+Abhijeet6394@users.noreply.github.com>
2022-01-02 19:39:19 +0530

    Add files via upload

---
+++ drive2.js
@@ -0,0 +1,17 @@
+let close_btn = document.getElementById('Grid_btn');
+close_btn.addEventListener('click', function(){
+    let sub_div = document.getElementById('sub_div11')
+    sub_div.classList.add('display_none')
+    let grid9 = document.getElementById('grid_1')
+    grid9.classList.add('display_show')
+})
+
+let show_flex_box = document.getElementById('Flex_btn')
+btn2.addEventListener('click', function(){
+    let grid_hide = document.getElementById('sub_div11')
+    grid_hide.classList.remove('display_none')
+    let show_flex = document.getElementById('grid_1')
+    show_flex.classList.remove('display_show')
+})
+
+
